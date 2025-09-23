4232ad1b6f4c5f41b311aa113bece0a238a842a9
Freaks <galleyhugo@icloud.com>
2025-09-23 14:54:46 +0200

    feat(Front) : AJout du Https

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './ssl/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './ssl/cert.pem')),
+    },
+    host: '0.0.0.0', // Permet l'accès depuis l'IP locale et Docker
+    port: 5173,
+  },
 })
